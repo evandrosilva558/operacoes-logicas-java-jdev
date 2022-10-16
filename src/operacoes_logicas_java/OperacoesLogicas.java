2073ff9ebf51cb66b917ef7e3272b71c8a2b6023
Evandro <evandrosilva558@hotmail.com>
2022-10-16 10:42:09 -0300

    operações lógicas media aluno com IF e ELSE

--- src/operacoes_logicas_java/OperacoesLogicas.java
+++ src/operacoes_logicas_java/OperacoesLogicas.java
@@ -3,7 +3,26 @@ package operacoes_logicas_java;
 public class OperacoesLogicas {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		int nota1 = 75;
+		int nota2 = 65;
+		int nota3 = 65;
+		int nota4 = 60;
+		int media;
+		
+		media = (nota1 + nota2 + nota3 + nota4)/4;
+
+		if (media >= 70) {
+			System.out.println("Aluno aprovado" + " " + media);
+		}
+
+		else if (media >= 60) {
+			System.out.println("Aluno em reculperação" + " " + media);
+		}
+
+		else {
+			System.out.println("Aluno reprovado" + " " + media);
+		}
 
 	}
 
